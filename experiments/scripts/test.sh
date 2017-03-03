22922d300e4b8c4a80b0a7e91ff18997404038aa
li526 <li526@indiana.edu>
2017-03-03 16:34:02 -0500

    update

---
+++ experiments/scripts/test.sh
@@ -0,0 +1,3 @@
+
+hdfs dfs -rm -r sahad-output/web-google/web-google-u-e-2/40threads
+./runHarpSgcr.sh $HARP_SAHAD_HOME/experiments/template/u5-1.t sahad-data/web-google/web-google-u-e-2 sahad-output/web-google/web-google-u-e-2/40threads 2 40
