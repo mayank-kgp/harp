2e1601941de644ea0941a364935576170b336773
li526 <li526@indiana.edu>
2017-04-26 17:45:14 -0400

    sync

---
+++ experiments/scripts/runfascia.sh
@@ -0,0 +1,6 @@
+if [ $# != 1 ] ; then
+echo "usage: $0 <the parameters> (must be quoted as one string)"
+exit 1;
+fi
+
+java -cp ~/harp-sahad/target/harp-sahad-1.0-SNAPSHOT.jar:/N/u/mengli/hadoop-2.6.0/share/hadoop/common/lib/* fascia.Fascia $1
