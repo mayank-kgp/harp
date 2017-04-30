97ef04c89ba062f06a7fc7ab48f8370bf16d3a47
li526 <li526@indiana.edu>
2017-04-30 02:54:38 -0400

    add script

--- experiments/scripts/runTest.sh
+++ experiments/scripts/runTest.sh
@@ -3,12 +3,12 @@ echo "USAGE: $0 e/m/r <dataset:miami/web-google/gnp1m100e>"
 echo " e.g.: $0 e web-google"
 exit 1;
 fi
-thread=40
-for t in 1 2
+thread=1
+for t in 1
 do
-	for temp in u3-1.t #u3-1.t  u5-1.t u7-1.t # u5-2.t u5-3.t #u3-1.t u5-1.t u7-1.t #  u5-1.t u7-1.t #u3-1.t u5-2.t u5-3.t #u10-1.t
+	for temp in  u5-1.t #u7-1.t # u5-2.t u5-3.t #u3-1.t u5-1.t u7-1.t #  u5-1.t u7-1.t #u3-1.t u5-2.t u5-3.t #u10-1.t
 	do
-		for i in  4   
+		for i in  1 
 	          do	
 			./runHarpSgcr.sh $HARP_SAHAD_HOME/experiments/template/$temp sahad-data/$2/$2-u-$1-$i sahad-output/$2/$2-u-$1-$i/$thread-threads/$t $i $thread
 			hdfs dfs -cat sahad-output/$2/$2-u-$1-$i/$thread-threads/$t/*
